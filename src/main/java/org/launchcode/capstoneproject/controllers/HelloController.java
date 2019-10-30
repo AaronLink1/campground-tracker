8c14f8295972030ce2c72bb1253cb328223634c0
Aaron Link <aaron@thelinkfamily.net>
2019-10-29 19:03:06 -0500

    Hello World 2.0 Commit

--- src/main/java/org/launchcode/capstoneproject/controllers/HelloController.java
+++ src/main/java/org/launchcode/capstoneproject/controllers/HelloController.java
@@ -2,13 +2,11 @@ package org.launchcode.capstoneproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @RequestMapping(value = "")
-    @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "login";
     }
 }
