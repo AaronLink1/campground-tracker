9877497d11bbcec8c57616d9cea4799f839bd6be
Aaron Link <aaron@thelinkfamily.net>
2019-11-19 20:02:40 -0600

    Added electric, water, dump and facilities to campground

--- src/main/java/org/launchcode/capstoneproject/models/Campground.java
+++ src/main/java/org/launchcode/capstoneproject/models/Campground.java
@@ -23,12 +23,29 @@ public class Campground {
     @NotNull
     private int price;
 
+    @NotNull
+    private Boolean hasElectric;
+
+    @NotNull
+    private Boolean hasWater;
+
+    @NotNull
+    private Boolean hasDump;
+
+    @NotNull
+    private Boolean hasFacilities;
+
     //Constructors
     public void Campground() {}
-    public void Campground(String name, String location, int price) {
+    public void Campground(String name, String location, int price, Boolean hasElectric, Boolean hasWater,
+                           Boolean hasDump, Boolean hasFacilities) {
         this.name = name;
         this.location = location;
         this.price = price;
+        this.hasElectric = hasElectric;
+        this.hasWater = hasWater;
+        this.hasDump = hasDump;
+        this.hasFacilities = hasFacilities;
     }
 
     //Getters
@@ -36,9 +53,17 @@ public class Campground {
     public String getName() { return name; }
     public String getLocation() { return location; }
     public int getPrice() { return price; }
+    public Boolean getHasElectric() { return hasElectric; }
+    public Boolean getHasWater() { return hasWater; }
+    public Boolean getHasDump() { return hasDump; }
+    public Boolean getHasFacilities() { return hasFacilities; }
 
     //Setters
     public void setName(String name) { this.name = name; }
     public void setLocation(String location) { this.location = location; }
     public void setPrice(int price) { this.price = price; }
+    public void setHasElectric(Boolean hasElectric) { this.hasElectric = hasElectric; }
+    public void setHasWater(Boolean hasWater) { this.hasWater = hasWater; }
+    public void setHasDump(Boolean hasDump) { this.hasDump = hasDump; }
+    public void setHasFacilities(Boolean hasFacilities) { this.hasFacilities = hasFacilities; }
 }
