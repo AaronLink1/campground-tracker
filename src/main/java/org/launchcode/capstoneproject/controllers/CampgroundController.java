0473cc84db48df5aaf05fcefa749e9664dfe6869
Aaron Link <aaron@thelinkfamily.net>
2019-12-07 01:26:15 -0600

    CampgroundController @"/campground" was updated

--- src/main/java/org/launchcode/capstoneproject/controllers/CampgroundController.java
+++ src/main/java/org/launchcode/capstoneproject/controllers/CampgroundController.java
@@ -1,16 +1,22 @@
 package org.launchcode.capstoneproject.controllers;
 
 import org.launchcode.capstoneproject.models.Campground;
+import org.launchcode.capstoneproject.models.User;
 import org.launchcode.capstoneproject.models.data.CampgroundDao;
+import org.launchcode.capstoneproject.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value = "campgrounds")
@@ -18,16 +24,27 @@ public class CampgroundController {
     @Autowired
     private CampgroundDao campgroundDao;
 
+    @Autowired
+    private UserDao userDao;
+
     @RequestMapping(value = "")
     public String index(Model model) {
-        model.addAttribute("title", "Campground");
-        model.addAttribute("campgrounds", campgroundDao.findAll());
+
+        //Get the current users details
+        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User user = userDao.findByUsername(userDetails.getUsername());
+        ArrayList<Campground> campgrounds = campgroundDao.findAllByUserId(user.getId());
+
+        model.addAttribute("title", "Campgrounds");
+        model.addAttribute("campgrounds", campgrounds);
         return "campgrounds/index";
     }
 
-    @RequestMapping(value = "add-campground", method = RequestMethod.GET)
-    public String addCampsite(Model model) {
+    @RequestMapping(value = "add-campground/{username}", method = RequestMethod.GET)
+    public String addCampsite(Model model, @PathVariable String username) {
         model.addAttribute("title", "Add Campground");
+        model.addAttribute("user", userDao.findByUsername(username));
         model.addAttribute(new Campground());
         return "campgrounds/add-campground";
     }
--- src/main/java/org/launchcode/capstoneproject/models/data/CampgroundDao.java
+++ src/main/java/org/launchcode/capstoneproject/models/data/CampgroundDao.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Repository
 @Transactional
 public interface CampgroundDao extends JpaRepository<Campground, Integer> {
     //Campground findByCampground(String campground);
+    ArrayList<Campground> findAllByUserId(long user_id);
 }
