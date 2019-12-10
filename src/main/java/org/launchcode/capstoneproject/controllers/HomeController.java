68bfd6437c86671f7a70745fe05e89f7fe5f0eea
Aaron Link <aaron@thelinkfamily.net>
2019-12-10 12:48:35 -0600

    finished home page

--- src/main/java/org/launchcode/capstoneproject/controllers/HomeController.java
+++ src/main/java/org/launchcode/capstoneproject/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package org.launchcode.capstoneproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "Campground Tracker");
         return "home/index";
     }
 }
